fc54375033f00bb3e5d63a911779f8413df02bef
quf(Hustp) <quf@hustp.com>
2019-01-26 17:37:47 +0800

    modify setting

--- tuotuo/settings.py
+++ tuotuo/settings.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = 'tuotuo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tuotuo',
+        'USER': 'root',
+        'PASSWORD': '585822',
+        'HOST': '118.24.60.9',
+        'PORT': '3306'
     }
 }
 
@@ -105,15 +115,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
