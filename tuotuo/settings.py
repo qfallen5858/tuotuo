539056bf1ae79129ee88d96910e588f9303e4818
qufan <qufan@home.com>
2019-01-26 23:07:26 +0800

    add polls

--- tuotuo/settings.py
+++ tuotuo/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_web'
+    'django_web',
+    'polls.apps.PollsConfig'
 ]
 
 MIDDLEWARE = [
--- tuotuo/urls.py
+++ tuotuo/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django_web.views import index
 
 
+
 urlpatterns = [
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
     path('index/', index)
 ]
